7b2f292ca50d70e15ca661e0219fcfcd8663da34
yaner <1468275133@qq.com>
2022-02-07 15:57:37 +0800

    大改定位逻辑,初步实现定位功能

--- app/src/main/java/com/elab/elabsignup/LocationListener.java
+++ app/src/main/java/com/elab/elabsignup/LocationListener.java
@@ -4,10 +4,10 @@ import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 
 public class LocationListener extends BDAbstractLocationListener {
-    Double latitude; // 纬度
-    Double longitude; // 经度
-    Float radius; // 定位精确度半径
-    int errorCode; // 错误代码
+    public double latitude; // 纬度
+    public double longitude; // 经度
+    public float radius; // 定位精确度半径
+    public int errorCode; // 错误代码
     @Override public void onReceiveLocation(BDLocation location){
         this.latitude = location.getLatitude();
         this.longitude = location.getLongitude();
